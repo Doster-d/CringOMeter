b96016cb107233c4b8726e09ff655a0a8d4732af
Igor Spichkin <igorsa.ux@outlook.com>
2021-07-10 22:24:54 +0300

    Initial commit

---
+++ CringOMeter.Wasm/Shared/Label.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CringOMeter.Wasm.Shared
+{
+    /// <summary>
+    ///     Github Label.
+    /// </summary>
+    public sealed class Label
+    {
+        /// <summary>
+        ///     Название плашки.
+        /// </summary>
+        public string Name { get; init; } = string.Empty;
+        /// <summary>
+        ///     Описание плашки.
+        /// </summary>
+        public string Description { get; init; } = string.Empty;
+        /// <summary>
+        ///     Цвет плашки.
+        /// </summary>
+        public string Color { get; init; } = string.Empty;
+    }
+}
---
+++ CringOMeter.Wasm/Shared/User.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text.Json.Serialization;
+using System.Threading.Tasks;
+
+namespace CringOMeter.Wasm.Shared
+{
+    /// <summary>
+    ///     Пользователь Github.
+    /// </summary>
+    public sealed class User
+    {
+        /// <summary>
+        ///     Логин пользователя.
+        /// </summary>
+        public string Login { get; init; } = string.Empty;
+
+        /// <summary>
+        ///     Ссылка на аватар пользователя.
+        /// </summary>
+        [JsonPropertyName("avatar_url")]
+        public string Avatar { get; init; } = string.Empty;
+    }
+}
